99a3a3e711c15660e628a3c7aa69117d85f43015
+collins <+collinskipngetich60@gmail.com>
2024-04-13 16:47:22 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,26 +1,57 @@
-from flask import Flask, request, make_response, jsonify
-from flask_cors import CORS
-from flask_migrate import Migrate
+# server/app.py
 
-from models import db, Message
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
+db = SQLAlchemy(app)
 CORS(app)
-migrate = Migrate(app, db)
 
-db.init_app(app)
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(255), nullable=False)
+    username = db.Column(db.String(50), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-@app.route('/messages')
-def messages():
-    return ''
+    def __repr__(self):
+        return '<Message %r>' % self.id
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([message.__dict__ for message in messages])
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    new_message = Message(body=data.get('body'), username=data.get('username'))
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.__dict__)
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get(id)
+    if message:
+        data = request.get_json()
+        message.body = data.get('body', message.body)
+        db.session.commit()
+        return jsonify(message.__dict__)
+    return jsonify({'message': 'Message not found'}), 404
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get(id)
+    if message:
+        db.session.delete(message)
+        db.session.commit()
+        return jsonify({'message': 'Message deleted'})
+    return jsonify({'message': 'Message not found'}), 404
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(debug=True)
